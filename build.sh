b897712eb6bc97a54c8198d8e48cf5c9cbd95cfd
Guillaume Genty <ggenty@waycom.net>
2023-12-11 20:13:55 +0100

    Still some errors, but partclone is started

--- build.sh
+++ build.sh
@@ -23,8 +23,21 @@ unxz --verbose -kc cache/init.xz > /tmp/fos-init
 LOOPINIT=$(losetup --show -f /tmp/fos-init)
 MOUNTINIT=$(mktemp -d)
 mount $LOOPINIT $MOUNTINIT
+if [ $? -ne 0 ]; then
+    echo Failed to mount $MOUNTINIT
+    exit 1
+fi
 cp -prv patch/* $MOUNTINIT/
+sync -f $MOUNTINIT
 umount $MOUNTINIT
+if [ $? -ne 0 ]; then
+    sleep 1
+    umount $MOUNTINIT
+    if [ $? -ne 0 ]; then
+        echo Failed to unmount $MOUNTINIT
+        exit 1
+    fi
+fi
 rmdir $MOUNTINIT
 losetup -d $LOOPINIT
 xz --check=crc32 --verbose -kc /tmp/fos-init > cache/custom-init.xz
@@ -33,7 +46,7 @@ echo Make a blank disk image
 dd if=/dev/zero of=cache/fos-usb.img bs=1M count=256
  
 echo Make the partition table, partition and set it bootable.
-parted --script cache/fos-usb.img mklabel msdos mkpart primary fat32 1 128 set 1 boot on mkpart primary ext2 129 256
+parted --script cache/fos-usb.img mklabel msdos mkpart primary ext2 1 128 set 1 boot on mkpart primary ntfs 129 256
 
 echo Map the partitions from the image file
 kpartx -a -s cache/fos-usb.img
@@ -45,13 +58,17 @@ if [ -z "${LOOPDEV}" ]; then
 fi
  
 echo Make filesystems
-mkfs -t vfat -n GRUB /dev/mapper/${LOOPDEV}p1
-mkfs -t ext2 -L IMAGES /dev/mapper/${LOOPDEV}p2
+mkfs -t ext2 -L GRUB /dev/mapper/${LOOPDEV}p1
+mkfs -t ntfs -L IMAGES /dev/mapper/${LOOPDEV}p2
 # fstab: LABEL=IMAGES
  
 echo Mount the filesystem via loopback
 MOUNT=$(mktemp -d)
 mount /dev/mapper/${LOOPDEV}p1 $MOUNT
+if [ $? -ne 0 ]; then
+    echo Failed to mount $MOUNT
+    exit 1
+fi
 
 echo Install GRUB
 #grub-install --removable --no-nvram --no-uefi-secure-boot --efi-directory=$MOUNT --boot-directory=$MOUNT/boot --target=i386-efi
@@ -78,7 +95,15 @@ menuentry "FOG Deploy from USB" {
 
 menuentry "[DEBUG] FOG Deploy from USB" {
  echo loading kernel
- linux /boot/bzImage loglevel=7 initrd=custom-init.xz root=/dev/ram0 rw ramdisk_size=275000 keymap= boottype=usb consoleblank=0 rootfstype=ext4 console=tty0 console=ttyS0,115200n8 isdebug=yes
+ linux /boot/bzImage loglevel=7 initrd=custom-init.xz root=/dev/ram0 rw ramdisk_size=275000 keymap= boottype=usb consoleblank=0 rootfstype=ext4 isdebug=yes
+ echo loading ram disk
+ initrd /boot/custom-init.xz
+ echo booting kernel...
+}
+
+menuentry "[DEBUG] FOG Deploy with serial console" {
+ echo loading kernel
+ linux /boot/bzImage loglevel=7 initrd=custom-init.xz root=/dev/ram0 rw ramdisk_size=275000 keymap= boottype=usb consoleblank=0 rootfstype=ext4 console=ttyS0,115200n8 isdebug=yes
  echo loading ram disk
  initrd /boot/custom-init.xz
  echo booting kernel...
@@ -87,7 +112,16 @@ menuentry "[DEBUG] FOG Deploy from USB" {
 EOF
  
 echo Unmount the loopback
+sync -f $MOUNT
 umount $MOUNT
+if [ $? -ne 0 ]; then
+    sleep 1
+    umount $MOUNT
+    if [ $? -ne 0 ]; then
+        echo Failed to unmount $MOUNT
+        exit 1
+    fi
+fi
 rmdir $MOUNT
  
 echo Unmap the image
--- write.sh
+++ write.sh
@@ -5,6 +5,10 @@ if [ "$EUID" -ne 0 ]; then
     exit
 fi
 
+if [ -z "$1" ]; then
+    echo "Missing target device."
+    exit
+fi
 
 echo "WILL ERASE $1 NOW!!!"
 echo "Press CTRL+C to cancel..."
@@ -12,9 +16,15 @@ echo "(Starting in 3s)"
 
 sleep 3
 
+mount | grep "/media" | grep $1 | cut -d" " -f 1 | xargs -r -n1 umount
+
 echo "Writing..."
 dd bs=1M if=cache/fos-usb.img of=$1 status=progress
 
 parted --script $1 resizepart 2 100%
-#or: growpart $1 2
-resize2fs ${1}2
+
+echo Waiting for ${1}2 to appear...
+until [ -f ${1}2 ]; do sleep 1; done
+echo OK
+
+ntfsresize -f ${1}2
